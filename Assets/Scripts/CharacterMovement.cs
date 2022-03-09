45326956d5fa69f5fb643c0069db529baa948d2c
sednaman <75589522+sednaman@users.noreply.github.com>
2022-03-08 23:11:38 -0600

    Fixed movement script

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -8,10 +8,12 @@ public class CharacterMovement : MonoBehaviour
 
     private Rigidbody2D rb; 
     private Vector3 moveDir;
-    bool movingLeftRight, movingUpDown;
+    bool movingRight, movingLeft, movingForward, movingBackward, moving;
     GameObject sprite;
     GameObject r0, r1, r2, r3, r4, r5, r6, r7;
     int lastSprite, currentSprite, playerState;
+    float moveX;
+    float moveY;
 
     void Start()
     {
@@ -36,70 +38,102 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update(){
 
-        float moveX = 0f;
-        float moveY = 0f;
+        moveX = 0f;
+        moveY = 0f;
 
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)){
-           moveY = +1f;
-           if (!movingLeftRight)
-           {
-               ChangeSprite(6);
-           }
-           movingLeftRight = true;
-           playerState = 0;
+        MovementCheck();
+        if (movingRight)
+        {
+            if (!moving)
+            {
+                ChangeSprite(7);
+            }
+            moving = true;
+            playerState = 0;
         }
-        else if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)){
-           moveY = -1f;
-           if (!movingLeftRight)
-           {
-               ChangeSprite(5);
-           }
-           movingLeftRight = true;
-           playerState = 1;
+        else if (movingLeft)
+        {
+            if (!moving)
+            {
+                ChangeSprite(4);
+            }
+            moving = true;
+            playerState = 1;
+        }
+        else if (movingForward)
+        {
+            if (!moving)
+            {
+                ChangeSprite(6);
+            }
+            moving = true;
+            playerState = 2;
+        }
+        else if (movingBackward)
+        {
+            if (!moving)
+            {
+                ChangeSprite(5);
+            }
+            moving = true;
+            playerState = 3;
         }
         else
         {
-            movingLeftRight = false;
-            switch (playerState)
+            if (moving)
             {
-                case 0: ChangeSprite(2); break;
-                case 1: ChangeSprite(1); break;
-                case 2: ChangeSprite(0); break;
-                case 3: ChangeSprite(3); break;
+                switch (playerState)
+                {
+                    case 0:
+                        ChangeSprite(3); break;
+                    case 1:
+                        ChangeSprite(0); break;
+                    case 2:
+                        ChangeSprite(2); break;
+                    case 3:
+                        ChangeSprite(1); break;
+                }
             }
-        }
-
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
-           moveX = -1f;
-           if (!movingUpDown)
-           {
-               ChangeSprite(4);
-           }
-           movingUpDown = true;
-           playerState = 2;
-        }
-        else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
-           moveX = +1f;
-           if (!movingUpDown)
-           {
-               ChangeSprite(7);
-           }
-           movingUpDown = true;
-           playerState = 3;
-        }
-        else
-        {
-            movingUpDown = false;
-            switch (playerState)
-            {
-                case 0: ChangeSprite(2); break;
-                case 1: ChangeSprite(1); break;
-                case 2: ChangeSprite(0); break;
-                case 3: ChangeSprite(3); break;
-            }
+            moving = false;
         }
 
         moveDir = new Vector3(moveX,moveY);
+    }
+
+    void MovementCheck()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            moveY = +1f;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            moveY = -1f;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveX = -1f;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            moveX = +1f;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+            movingRight = true;
+        if (Input.GetKeyUp(KeyCode.D))
+            movingRight = false;
+        if (Input.GetKeyDown(KeyCode.A))
+            movingLeft = true;
+        if (Input.GetKeyUp(KeyCode.A))
+            movingLeft = false;
+        if (Input.GetKeyDown(KeyCode.S))
+            movingBackward = true;
+        if (Input.GetKeyUp(KeyCode.S))
+            movingBackward = false;
+        if (Input.GetKeyDown(KeyCode.W))
+            movingForward = true;
+        if (Input.GetKeyUp(KeyCode.W))
+            movingForward = false;
     }
 
     void ChangeSprite(int i)
