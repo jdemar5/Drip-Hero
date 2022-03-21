6758a2a0c995e32a377d45ce2a48bbeca706a86d
sednaman <75589522+sednaman@users.noreply.github.com>
2022-03-21 18:00:32 -0500

    Added cooldown functionality to weapons

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -11,8 +11,10 @@ public class Sword: MonoBehaviour
     private Rigidbody2D rb;
     Vector3 mousePos;
     float angle;
-    bool isSwordLeft;
+    private bool isSwordLeft, onCooldown;
     private AudioSource audioSource;
+    private Weapon weapon;
+    private float cooldownTime;
 
 
     void OnTriggerEnter(Collider other)
@@ -26,13 +28,16 @@ public class Sword: MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody2D>();
         isSwordLeft = true;
         audioSource = this.GetComponent<AudioSource>();
+        weapon = GameObject.Find("Weapon").GetComponent<Weapon>();
+        cooldownTime = weapon.GetCooldownTime();
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !onCooldown)
         {
             audioSource.Play();
+            StartCoroutine("Cooldown");
             if (isSwordLeft)
             {
                 isSwordLeft = false;
@@ -57,5 +62,12 @@ public class Sword: MonoBehaviour
             Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
             rb.MoveRotation(rotation);
         }
+    }
+    
+    IEnumerator Cooldown()
+    {
+        onCooldown = true;
+        yield return new WaitForSeconds(cooldownTime);
+        onCooldown = false;
     }
 }
